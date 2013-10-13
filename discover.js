140d32b802e0d4b0feab73abc504b1bda0ed4e38
Glen Maddern <glenmaddern@gmail.com>
2013-10-13 15:14:55 +1100

    shipping it

--- discover.js
+++ discover.js
@@ -2,30 +2,19 @@
 var mdns = require('mdns');
 
 // advertise a http server on port 54127
-var ad = mdns.createAdvertisement(mdns.tcp('salut'), 5412, {txtRecord: "LOL"});
+var ad = mdns.createAdvertisement(mdns.tcp('salut'), 54127);
 ad.start();
 
-process.on('exit', function() {
+process.on('SIGINT', function() {
   ad.stop();
   console.log('About to exit.');
 });
 
-// watch all http servers
-//var browser = mdns.createBrowser(mdns.tcp('http'));
-//browser.on('serviceUp', function(service) {
-//  console.log("service up: ", service);
-//});
-//browser.on('serviceDown', function(service) {
-//  console.log("service down: ", service);
-//});
-//browser.start();
-//
-// discover all available service types
-//var browser = mdns.browseThemAll(); // all_the_types is just another browser...
-//browser.on('serviceUp', function(service) {
-//  console.log("service up: ", service);
-//});
-//browser.on('serviceDown', function(service) {
-//  console.log("service down: ", service);
-//});
-//browser.start();
+var browser = mdns.createBrowser(mdns.tcp('salut'));
+browser.on('serviceUp', function(service) {
+  console.log("service up: ", service);
+});
+browser.on('serviceDown', function(service) {
+  console.log("service down: ", service);
+});
+browser.start();
