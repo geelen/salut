a7220fbd35e82a399b7d7c4de42a27804668467a
Glen Maddern <glenmaddern@gmail.com>
2013-10-13 15:54:25 +1100

    ok we are communicating with ourselves!!

--- discover.js
+++ discover.js
@@ -1,21 +1,54 @@
 // import the module
-var mdns = require('mdns');
+var mdns = require('mdns'),
+  net = require('net');
 
 // advertise a http server on port 54127
 var ad = mdns.createAdvertisement(mdns.tcp('salut'), 54127);
 ad.start();
 
+var clients = {};
+
+var client = function (service) {
+  return {
+    service: service,
+    socket: net.createConnection({host: service.host, port: service.port})
+      .on('connect', function () {
+        console.log("[" + service.host + "] CONNECTED");
+        this.write("WHATUP!");
+      })
+      .on('data', function (data) {
+        console.log("[" + service.host + "] " + data.toString());
+      })
+  }
+};
+
 var browser = mdns.createBrowser(mdns.tcp('salut'));
-browser.on('serviceUp', function(service) {
+browser.on('serviceUp', function (service) {
   console.log("service up: ", service);
+  clients[service.fullname] = client(service);
 });
-browser.on('serviceDown', function(service) {
+browser.on('serviceDown', function (service) {
+  clients[service.fullname].socket.disconnect();
+  delete clients[service.fullname];
   console.log("service down: ", service);
 });
 browser.start();
 
-process.on('SIGINT', function() {
+
+var server = net.createServer(function (socket) {
+  socket.on("data", function(data) {
+    console.log("DAT DATA: " + data.toString());
+    socket.write("OK YOU FUCKERS")
+  })
+  console.log("Got us a socket!");
+}).listen(54127);
+
+process.on('SIGINT', function () {
+  Object.keys(clients).forEach(function(k) {
+    clients[k].socket.end();
+  });
   ad.stop();
   browser.stop();
+  server.close();
   console.log('About to exit.');
 });
