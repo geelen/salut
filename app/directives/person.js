fb8eae9d57873fc88c1403646b8c0effe93e7263
Glen Maddern <glenmaddern@gmail.com>
2013-10-14 15:14:44 +1100

    Better styling

--- app/directives/person.js
+++ app/directives/person.js
@@ -2,8 +2,9 @@
 
   app.directive('person', function () {
     return {
+      transclude: true,
       restrict: 'E',
-      template: "<div class=\"person\">\n  <img ng-src=\"{{ person.avatar }}\" alt=\"{{ person.username }}\"/>\n  {{ person.username }}\n</div>",
+      template: "<div class=\"person\">\n  <img ng-src=\"{{ person.avatar }}\" alt=\"{{ person.username }}\"/>\n  {{ person.username }} <span ng-transclude></span>\n</div>",
       scope: {person: '=who'}
     }
   });
